4ddf816a5e884b3a9d094c6613daaf31590efd9d
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-12-11 19:16:42 -0500

    link

---
+++ build/precache-manifest.294a46b39cfd694be5a5a1a1946ee45d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcee4224f99953fe906e9ef15de12b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3603d829f59b14e8d8a",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "279eb9045a9b74c455a3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c3603d829f59b14e8d8a",
+    "url": "/static/js/2.afec906b.chunk.js"
+  },
+  {
+    "revision": "279eb9045a9b74c455a3",
+    "url": "/static/js/main.29d66101.chunk.js"
+  },
+  {
+    "revision": "6c2f931dd55ac143d7cd",
+    "url": "/static/js/runtime-main.d9d90118.js"
+  }
+]);
